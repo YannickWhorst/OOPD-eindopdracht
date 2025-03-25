7bbdaf2c74a3a5026745e964b2aa3a554f9faab6
YannickWhorst <42058367+YannickWhorst@users.noreply.github.com>
2025-03-25 18:38:36 +0100

    Add StartButton class and integrate into TitleScene

--- src/main/java/main/entities/buttons/StartButton.java
+++ src/main/java/main/entities/buttons/StartButton.java
@@ -1,4 +1,17 @@
 package main.entities.buttons;
 
-public class StartButton {
+import com.github.hanyaeger.api.AnchorPoint;
+import com.github.hanyaeger.api.Coordinate2D;
+import com.github.hanyaeger.api.entities.impl.TextEntity;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+
+public class StartButton extends TextEntity {
+    public StartButton(Coordinate2D initialLocation) {
+        super(initialLocation, "Start");
+        setFill(Color.PURPLE);
+        setFont(Font.font("Roboto", FontWeight.BOLD, 20));
+        setAnchorPoint(AnchorPoint.CENTER_CENTER);
+    }
 }
--- src/main/java/main/scene/TitleScene.java
+++ src/main/java/main/scene/TitleScene.java
@@ -7,6 +7,7 @@ import com.github.hanyaeger.api.scenes.StaticScene;
 import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
+import main.entities.buttons.StartButton;
 
 public class TitleScene extends StaticScene {
     @Override
@@ -16,13 +17,15 @@ public class TitleScene extends StaticScene {
 
     @Override
     public void setupEntities() {
-        var waterworldText = new TextEntity(
+        var towerShowerText = new TextEntity(
                 new Coordinate2D(getWidth() / 2, 100),
                 "Tower Shower"
         );
-        waterworldText.setAnchorPoint(AnchorPoint.CENTER_CENTER);
-        waterworldText.setFill(Color.BLUE);
-        waterworldText.setFont(Font.font("Roboto", FontWeight.SEMI_BOLD, 80));
-        addEntity(waterworldText);
+        towerShowerText.setAnchorPoint(AnchorPoint.CENTER_CENTER);
+        towerShowerText.setFill(Color.BLUE);
+        towerShowerText.setFont(Font.font("Roboto", FontWeight.SEMI_BOLD, 80));
+        addEntity(towerShowerText);
+
+        addEntity(new StartButton(new Coordinate2D(getWidth() / 2, 200)));
     }
 }
